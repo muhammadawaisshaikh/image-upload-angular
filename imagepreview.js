dd655682ef67aa460045f5eef8a801f5e97682ea
Muhammad Awais <awaisshaikh94@gmail.com>
2019-08-22 16:45:21 +0500

    resolved typescript issues
    reader.result as string and HTMLInputElement

--- imagepreview.js
+++ imagepreview.js
@@ -1,13 +1,25 @@
 mgArea: string = 'https://i.ibb.co/z7nrQsn/047-Fly-High.png';
 imageSrc: string;
   
-  // read image onChange
+  // read image onChange Javascript
   readURL(event: Event): void {
     if (event.target.files && event.target.files[0]) {
       const file = event.target.files[0];
 
       const reader = new FileReader();
       reader.onload = e => this.imageSrc = reader.result;
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+// read image onChange Typerscript
+  readURL(event: Event): void {
+    if ((<HTMLInputElement>event.target).files && (<HTMLInputElement>event.target).files[0]) {
+      const file = (<HTMLInputElement>event.target).files[0];
+
+      const reader = new FileReader();
+      reader.onload = e => this.imageSrc = reader.result as string;
 
       reader.readAsDataURL(file);
     }
